6912ef3e5a7b3988bbad1637cbe14f30f8903415
shaharsprontz <sh.sprontz@gmail.com>
2021-06-28 08:51:39 +0300

    a

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('fox-store').then((cache) => cache.addAll([
+      'a2hs/',
+      'a2hs/index.html',
+      'a2hs/index.js',
+      'a2hs/style.css',
+      'a2hs/images/fox1.jpg',
+      'a2hs/images/fox2.jpg',
+      'a2hs/images/fox3.jpg',
+      'a2hs/images/fox4.jpg',
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
