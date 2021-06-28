8d66701c1b241740c79d4bc0cc0c85f2a7d93814
shaharsprontz <sh.sprontz@gmail.com>
2021-06-28 09:12:31 +0300

    q

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ setInterval(() => {
 
 // Register service worker to control making site work offline
 
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker
-//     .register('sw.js')
-//     .then(() => { console.log('Service Worker Registered'); });
-// }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('sw.js')
+    .then(() => { console.log('Service Worker Registered'); });
+}
 
 // Code to handle install prompt on desktop
 
