156184b8e6a10b659bf7c3815ab0f816a665f627
shaharsprontz <sh.sprontz@gmail.com>
2021-06-28 09:31:52 +0300

    Remove beforeInstall method

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
 //addBtn.style.display = 'none';
 
-window.addEventListener('beforeinstallprompt', (e) => {
+// window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   // e.preventDefault();
   // Stash the event so it can be triggered later.
-  deferredPrompt = e;
+  // deferredPrompt = e;
   // Update UI to notify the user they can add to home screen
   addBtn.style.display = 'block';
 
@@ -48,4 +48,4 @@ window.addEventListener('beforeinstallprompt', (e) => {
       deferredPrompt = null;
     });
   });
-});
+// });
