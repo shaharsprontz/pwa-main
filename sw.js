42982f2668e6c4844b7c97298944a9c2bacdcf4a
shaharsprontz <sh.sprontz@gmail.com>
2021-06-28 09:18:11 +0300

    a

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('fox-store').then((cache) => cache.addAll([
+// self.addEventListener('install', (e) => {
+//   e.waitUntil(
+//     caches.open('fox-store').then((cache) => cache.addAll([
       
-      'pwa-main/index.html',
-      'pwa-main/index.js',
-      'pwa-main/style.css',
-      'pwa-main/images/fox1.jpg',
-      'pwa-main/images/fox2.jpg',
-      'pwa-main/images/fox3.jpg',
-      'pwa-main/images/fox4.jpg',
-    ])),
-  );
-});
+//       'pwa-main/index.html',
+//       'pwa-main/index.js',
+//       'pwa-main/style.css',
+//       'pwa-main/images/fox1.jpg',
+//       'pwa-main/images/fox2.jpg',
+//       'pwa-main/images/fox3.jpg',
+//       'pwa-main/images/fox4.jpg',
+//     ])),
+//   );
+// });
 
 self.addEventListener('fetch', (e) => {
   console.log(e.request.url);
